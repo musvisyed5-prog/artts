dfd93d6b38cdf5bf8dc6d343510e955cd8f16c4a
musvisyed5-prog <musvisyed5-prog@users.noreply.github.com>
2026-07-04 12:32:44 +0530

    Add Vercel deployment configuration

---
+++ vercel_app.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+# Add the 'src' directory to the python path so that Django can find 'src.settings'
+current_dir = os.path.dirname(os.path.abspath(__file__))
+src_dir = os.path.join(current_dir, 'src')
+if src_dir not in sys.path:
+    sys.path.insert(0, src_dir)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
+
+from django.core.wsgi import get_wsgi_application
+app = get_wsgi_application()
